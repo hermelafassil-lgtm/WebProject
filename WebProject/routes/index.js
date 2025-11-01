51205601ccd496f029514e5543dd14cb8a03cbc0
hermelafassil-lgtm <hermela.fassil@ontariotechu.net>
2025-11-01 16:36:07 -0400

    Initial commit

---
+++ WebProject/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home Page' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET home page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products' });
+});
+
+/* GET home page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET home page. */
+router.get('/contactme', function(req, res, next) {
+  res.render('index', { title: 'Contact me' });
+});
+
+module.exports = router;
+
