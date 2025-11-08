f10b82a241e061958cabb89cec1f7505fabe8743
hermelafassil-lgtm <hermela.fassil@ontariotechu.net>
2025-11-07 22:45:24 -0500

    Add  README.md and update app.js

--- WebProject/app.js
+++ WebProject/app.js
@@ -21,7 +21,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Makes the express makes dirname public
 app.use(express.static(path.join(__dirname, 'public')));
+// Makes the dirname node_modules
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 
